b19af81b31eb2ca4b1f7c20fc2db6d0944ca6416
14imran <14.imran.mohammed@gmail.com>
2020-10-20 21:03:25 +0530

    integrated features with branches

---
+++ routes/BlogsArticles.routes.js
@@ -0,0 +1,59 @@
+const express = require('express')
+const Article = require('../models/Blogsarticle.model')
+const router = express.Router()
+
+router.get('/', async(req, res) => {
+  const articles = await Article.find().sort({ createdAt: 'desc' })
+    res.render('blogs/articles/index', { articles: articles })
+  // res.render('blogs/articles/index', { article:new Article()})
+
+})
+
+
+router.get('/new', (req, res) => {
+  res.render('blogs/articles/new', { article: new Article() })
+})
+
+router.get('/edit/:id', async (req, res) => {
+  const article = await Article.findById(req.params.id)
+  res.render('blogs/articles/edit', { article: article })
+})
+
+router.get('/:slug', async (req, res) => {
+  const article = await Article.findOne({ slug: req.params.slug })
+  if (article == null) res.redirect('/articles')
+  res.render('blogs/articles/show', { article: article })
+})
+
+router.post('/', async (req, res, next) => {
+  console.log("data",req.body)
+  req.article = new Article()
+  next()
+}, saveArticleAndRedirect('new'))
+
+router.put('/edit/:id', async (req, res, next) => {
+  req.article = await Article.findByIdAndUpdate({_id:req.params.id},req.body)
+  next()
+}, saveArticleAndRedirect('edit'))
+
+router.get('/:id/delete', async (req, res) => {
+  await Article.findByIdAndDelete(req.params.id)
+  res.redirect('/articles')
+})
+
+function saveArticleAndRedirect(path) {
+  return async (req, res) => {
+    let article = req.article
+    article.title = req.body.title
+    article.description = req.body.description
+    article.markdown = req.body.markdown
+    try {
+      article = await article.save()
+      res.redirect(`/articles`)
+    } catch (e) {
+      res.render(`articles/${path}`, { article: article })
+    }
+  }
+}
+
+module.exports = router
