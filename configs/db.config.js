65dabb0583459bcaf60575db73d678cbb183c713
14imran <14.imran.mohammed@gmail.com>
2020-10-22 22:49:41 +0530

    changes

--- configs/db.config.js
+++ configs/db.config.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 mongoose
-  .connect("mongodb+srv://imran:abc12345678@cluster0.fhlcu.mongodb.net/droad?retryWrites=true&w=majority", {
+  .connect("nope", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
